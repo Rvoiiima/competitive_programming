feb11944dddffed5978bb271c273f96c7b71f529
RyoIijima <ryo@nsl.cs.waseda.ac.jp>
2019-03-20 23:11:24 +0900

    fix template

--- atcoder/abc/template_cpp.cpp
+++ atcoder/abc/template_cpp.cpp
@@ -23,32 +23,5 @@ typedef long long unsigned int ll;
 #define INF (1e9)
 #define PI (acos(-1))
 
-ll conv(string num) { stringstream ss; ss << num << flush; ll n; ss>>n; return n; }
-
-string conv(ll n) { stringstream ss; ss << n << flush; return ss.str(); }
-
-vector<string> split(string s, string delim) {
-    vector<string> res;
-    int pos = 0;
-    while(true) {
-        int found = s.find(delim, pos);
-        if(found >= 0) {
-            res.push_back(s.substr(pos, found - pos));
-        }
-        else {
-            res.push_back(s.substr(pos));
-            break;
-        }
-        pos = found + delim.size();
-    }
-    return res;
-}
-int ALPH_SIZE = 26;
-char ALPH_L[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-char ALPH_U[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-
 int main() {
-
-
-    return 0;
 }
