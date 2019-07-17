6a4dbdd9c4d754701f7f7b2322e5bd0e4419dca9
RyoIijima <ryo@nsl.cs.waseda.ac.jp>
2019-07-17 10:56:12 +0900

    pc-concours

---
+++ pc-concours/18-pre-1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+#define MAXN 100001
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << abs(a-b) << endl;
+}
+
