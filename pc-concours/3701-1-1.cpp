772ca817db702169b9940f20ad264be40be140a7
RyoIijima <ryo@nsl.cs.waseda.ac.jp>
2019-07-22 16:05:45 +0900

    add 3701

---
+++ pc-concours/3701-1-1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+#define MAXN 100001
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " <<  a+b << endl;
+    cout << a << " - " << b << " = " <<  a-b << endl;
+    cout << a << " * " << b << " = " <<  a*b << endl;
+    cout << a << " / " << b << " = " <<  a/b << endl;
+    cout << a << " % " << b << " = " <<  a%b << endl;
+}
+
