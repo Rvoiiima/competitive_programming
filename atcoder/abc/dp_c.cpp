f2d458833a70ef91745cadaf889b9c7f45c87547
Ryo <ryo@nsl.cs.waseda.ac.jp>
2019-08-19 17:11:09 +0900

    modify gitignore

--- atcoder/abc/dp_c.cpp
+++ atcoder/abc/dp_c.cpp
@@ -26,8 +26,11 @@ int main() {
 
     for(ll i=1; i<n; i++) {
         dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + a[i][0];
-        dp[i][1] = max(dp[i-1][2], dp[i-1][0]) + a[i][1];
+        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + a[i][1];
         dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + a[i][2];
     }
-    cout << max({dp[n-1][0], dp[n-1][1], dp[n-1][2]}) << endl;
+
+    cout << max(dp[n-1][0], max(dp[n-1][1], dp[n-1][2]));
+
+
 }
