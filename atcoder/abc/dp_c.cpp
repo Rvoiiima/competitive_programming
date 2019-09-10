4c9103de43f76ee4b32c999f868d778ab3d51eea
Ryo <ryo@nsl.cs.waseda.ac.jp>
2019-09-10 23:57:49 +0900

    add_dpc

--- atcoder/abc/dp_c.cpp
+++ atcoder/abc/dp_c.cpp
@@ -5,32 +5,31 @@ typedef long long ll;
 #define EPS (1e-7)
 #define INF (1e9)
 #define PI (acos(-1))
-#define MAXN 100001
+#define MAXN 100010
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+const ll mod = 1e9+7;
 
-ll dp[MAXN][3];
-ll n;
-ll a[MAXN][3];
-ll maxv = -1;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
-
+int a[MAXN][3];
+int h[MAXN][3];
 int main() {
-
-    cin >> n;
-    for(ll i=0; i<n; i++) {
-        cin >> a[i][0] >> a[i][1] >> a[i][2];
+    int N;
+    cin >> N;
+    for(int i=0; i<N; ++i) {
+        for(int j=0; j<3; ++j) {
+            cin >> a[i][j];
+        }
     }
 
-    dp[0][0] = a[0][0];
-    dp[0][1] = a[0][1];
-    dp[0][2] = a[0][2];
-
-    for(ll i=1; i<n; i++) {
-        dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + a[i][0];
-        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + a[i][1];
-        dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + a[i][2];
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<3; ++j) {
+            for(int k=0; k<3; ++k) {
+                if(j!=k) chmax(h[i+1][j], h[i][k]+a[i][j]);
+            }
+        }
     }
 
-    cout << max(dp[n-1][0], max(dp[n-1][1], dp[n-1][2]));
-
-
+    cout << max(h[N][0], max(h[N][1], h[N][2])) << endl;
 }
