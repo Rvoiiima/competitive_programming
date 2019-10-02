1544de38a95b43177f3d212be61ced2aa47204ab
Ryo <ryo@nsl.cs.waseda.ac.jp>
2019-10-02 10:18:10 +0900

    modify temp

--- atcoder/abc/template_cpp.cpp
+++ atcoder/abc/template_cpp.cpp
@@ -5,10 +5,12 @@ typedef long long ll;
 #define EPS (1e-7)
 #define INF (1e9)
 #define PI (acos(-1))
-#define MAXN 100010
+#define MAXN 1000010
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 const ll mod = 1e9+7;
 
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
 int main() {
 }
