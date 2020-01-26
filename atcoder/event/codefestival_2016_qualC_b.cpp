41fb2f325b37ed4545b676cf284504ee7089b156
RyoIijima <ryo@nsl.cs.waseda.ac.jp>
2020-01-26 10:23:06 +0900

    0126

--- atcoder/event/codefestival_2016_qualC_b.cpp
+++ atcoder/event/codefestival_2016_qualC_b.cpp
@@ -1,23 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> P;
 
 #define EPS (1e-7)
 #define INF (1e9)
 #define PI (acos(-1))
-#define MAXN 1000010
+#define MAXN 110
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 const ll mod = 1e9+7;
 
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
-bool pairCompare(const P& firstElof, const P& secondElof)
-{
-    return firstElof.second < secondElof.second;
-}
-
+int a[MAXN];
 int main() {
+    int k, t;
+
+    cin >> k >> t;
+
+    int ma = 0;
+    rep(i, t) {
+        cin >> a[i];
+        ma = max(ma, a[i]);
+    }
+
+    int ans = max(ma-1-(k-ma), 0);
+    cout << ans << endl;
 }
 
--- atcoder/event/nikkei2019_2_qual_b.cpp
+++ atcoder/event/nikkei2019_2_qual_b.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> P;
 
 #define EPS (1e-7)
 #define INF (1e9)
@@ -13,11 +12,29 @@ const ll mod = 1e9+7;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
-bool pairCompare(const P& firstElof, const P& secondElof)
-{
-    return firstElof.second < secondElof.second;
-}
+int cnt[MAXN];
 
 int main() {
+    int n;
+    cin >> n;
+    int tmp;
+    int m = 0;
+    rep(i, n) {
+        cin >> tmp;
+        ++cnt[tmp];
+        m = max(m, tmp);
+    }
+
+    ll ans = 0;
+
+    for(int i=1; i<m; ++i) {
+        ll tmp2 = 1;
+        for(int k=0; k<cnt[i]; ++k) {
+            tmp *= cnt[i-1];
+        }
+        ans *= cnt[i-1];
+    }
+
+    cout << ans << endl;
 }
 
